4ff74be88f910b2854d56e6977b4cfe931dedb95
JitkaT <159491364+JitkaT@users.noreply.github.com>
2024-07-24 14:47:31 +0200

    Update Projekt_1.py

--- Projekt_1.py
+++ Projekt_1.py
@@ -9,14 +9,21 @@ email: jitka.ticha@gmail.com
 discord: jitkaticha
 
 """
+#Inputs form users:
 user_name = input("Your username: ")
 password = input("Your password: ")
 separator = "-"*40
 print(separator)
-registred_usernames = {"bob": "123", "ann": "pass123", "mike": "password123", "liz": "pass123"}
+
+#Registration of users:
+registered_usernames = {"bob": "123", "ann": "pass123", "mike": "password123", "liz": "pass123"}
+
+#Login check:
 if user_name in registred_usernames and registred_usernames[user_name] == password:
     print(f"Welcome to the app {user_name}!\nWe have 3 texts to be analyzed.")
     print(separator)
+
+#texts:    
     texts = ['''
 Situated about 10 miles west of Kemmerer,
 Fossil Butte is a ruggedly impressive
@@ -44,28 +51,36 @@ other freshwater genera and herring similar to those
 in modern oceans. Other fish such as paddlefish,
 garpike and stingray are also present.'''
     ]
+    
+   
     text_choice_for_user = input("Enter a number btw. 1 and 3 to select: ")
     print(separator)
-    if not text_choice_for_user.isnumeric():
+    
+    if not text_choice_for_user.isdigit():
         print("The value is not a number, program needs to finish")
     elif int(text_choice_for_user) in range (1,4):
         text_choice_nr = texts[int(text_choice_for_user) - 1]
         words = text_choice_nr.split()
         print (f"There are {len(words)} words in the selected text.")
+        
         uppercase_words = list()
         titlecase_words = list()
         lowercase_words = list()
         digit_words = list()
+        
         for word in words:
-            if word.istitle() and word.isalpha():
-                titlecase_words.append(word)         
-            if word.isupper() and word.isalpha():
-                uppercase_words.append(word)
-            if word.islower() and word.isalpha():
-                lowercase_words.append(word)
-            if word.isdigit():
-                digit_words.append(word)
-        sum_of_digit_words = sum(int(word) for word in words if word.isnumeric())             
+            clean_word = word.strip(",.?!")
+            if clean_word.istitle() and clean_word.isalpha():
+                titlecase_words.append(clean_word)         
+            if clean_word.isupper() and clean_word.isalpha():
+                uppercase_words.append(clean_word)
+            if clean_word.islower() and clean_word.isalpha():
+                lowercase_words.append(clean_word)
+            if clean_word.isdigit():
+                digit_words.append(clean_word)
+                
+        sum_of_digit_words = sum(int(word) for word in digit_words)
+        
         print (f"There are {len(titlecase_words)} titlecase words.")          
         print (f"There are {len(uppercase_words)} uppercase words.")          
         print(f"There are {len(lowercase_words)} lowercase words.")
@@ -73,19 +88,21 @@ garpike and stingray are also present.'''
         print(f"The sum of all the numbers {sum_of_digit_words}.")
     else:
         print("It is not a number btw 1 and 3, the program needs to end.")
+        
     print(separator)
     print("LEN|","OCCURENCES".center(30),"|NR.")
     print(separator)
-    lenght_of_words = [len(word) for word in words]
-    sorted_length_words = sorted(lenght_of_words)
+    
+    length_of_words = [len(word.strip(",.?!") for word in words]
     presence = {}
-    for lenght in sorted_length_words:
-        if lenght in presence:
-            presence[lenght] += 1
+    
+    for length in sorted_length_words:
+        if length in presence:
+            presence[length] += 1
         else: 
-            presence[lenght] = 1
-    for lenght, value in presence.items():
-        alignment_1 = str(lenght).rjust(2)
+            presence[length] = 1
+    for length, value in sorted(presence.items()):
+        alignment_1 = str(length).rjust(2)
         alignment_2 = "*" * value
         alignment_3 = str(alignment_2).ljust(30)
         print(alignment_1, "|", alignment_3, "|", value)
